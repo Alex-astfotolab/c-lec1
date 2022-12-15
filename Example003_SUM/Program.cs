b9dd3644581499d2e7f77d7ceb19f8dd5f1a21c6
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2022-12-15 14:01:31 +0400

    add EX003_SUM

---
+++ Example003_SUM/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Сложение двух чисел");
+Console.WriteLine("Введите первое число:");
+string first = Console.ReadLine();
+int a = Convert.ToInt32(first);
+Console.WriteLine("Введите второе число:");
+string second = Console.ReadLine();
+int b = Convert.ToInt32(second);
+int res = a +b;
+Console.WriteLine("Результат: " + res);
