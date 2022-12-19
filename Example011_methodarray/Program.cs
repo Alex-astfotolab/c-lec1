33d00511a84a7e90eeb2e8d22838ca98510a4424
Alex-astfotolab <119451879+Alex-astfotolab@users.noreply.github.com>
2022-12-19 09:48:01 +0400

    add Example_011

---
+++ Example011_methodarray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 41, 32, 33, 44, 15, 46, 37, 28 };
+int n = array.Length;
+int find = 46;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.Write(index);
+        break;
+    }
+    index++;
+}
